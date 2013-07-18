5042cc8e993fd1eee2877bd73f18e7136321b2fa
resparra <rafael.esaprra1@upr.edu>
2013-07-18 15:39:47 -0400

    Fix Dashboard Index

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -5,12 +5,12 @@ from reports.models import Report
 from dashboard.models import dash
 
 def index(request):
-	report_list = Report.objects.all()
-	context = {'report_list': report_list }
+	dash_list = dash.objects.all()
+	context = {'dash_list': dash_list }
 	return render(request, 'dashboard/test.html', context)
 
 def display(request, dash_id):
 	dash_board = get_object_or_404(dash, pk=dash_id)
 	selected_reports = dash_board.selection_list.all()
-	context =  {'selected_reports' : selected_reports }
+	context =  {'selected_reports' : selected_reports, 'dash_board' : dash_board,}
 	return render(request, 'dashboard/dash_list.html', context)
