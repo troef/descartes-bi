f6f92cff760d351c7c363f5da739db3618be7259
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-08-02 16:00:26 -0400

    Moved website view logic from common to website app

--- apps/website/views.py
+++ apps/website/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def get_website(request, website):
+    context = {}
+
+    #Compose the URL to create LQL queries
+    if website.series:
+        url = website.series.data_source.load_backend().cursor().url
+        query = website.series.query
+        context['url'] = url + "/?" + query
+
+    #Display the website in base_URL
+    else:
+        context['url'] = website.base_URL
+
+    page = 'website/website.html'
+
+    return render_to_response(page, context,
+                              context_instance=RequestContext(request))
