433c0e26269ce7481acfa483524248603c4c57f0
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-08-06 14:23:15 -0400

    Changed website filtersets to be many2many and added get_filterform to website view.

--- apps/website/views.py
+++ apps/website/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from reports.forms import FilterForm
+
+import sys
+
 
 def get_website(request, website):
     context = {}
@@ -17,5 +21,21 @@ def get_website(request, website):
 
     page = 'website/website.html'
 
+    get_filter_form(request, website)
+
     return render_to_response(page, context,
+                              context_instance=RequestContext(request))
+
+
+def get_filter_form(request, website):
+    print >>sys.stderr, "Inside Get"
+    url = website.series.data_source.load_backend().cursor().url
+    query = website.series.query
+
+    filter_form = FilterForm(website.filterset.all(), request.user)
+    print >>sys.stderr, "got the filter_form"
+
+    return render_to_response('website/filter_website.html',
+                              {'filter_form': filter_form,
+                               'url': url + "/?" + query},
                               context_instance=RequestContext(request))
