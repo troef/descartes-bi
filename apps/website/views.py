71b67cbc736eaf310a7eee909b6b1bd0a36a6a7c
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-08-08 20:41:30 -0400

    Fixed views for websites with pre-rendered queries

--- apps/website/views.py
+++ apps/website/views.py
@@ -3,26 +3,25 @@ from django.template import RequestContext
 
 from reports.forms import FilterForm
 
-import sys
-
 
 def get_website(request, website):
     context = {}
-
     #Compose the URL to create LQL queries
     if website.series:
-
         url = website.series.data_source.load_backend().cursor().url
         query = website.series.query
 
-        if request.method == 'GET':
+        if request.method == 'GET' and website.filterset.exists():
             if request.GET:
                 filter_form = FilterForm(website.filterset.all(), request.user, request.GET)
-                query = query % filter_form               
+                query = query % filter_form
+                context['url'] = url + "/?" + query
             else:
                 filter_form = FilterForm(website.filterset.all(), request.user)
-                
-        context['url'] = url + "/?" + query
+
+            context['filter_form'] = filter_form
+        else:
+            context['url'] = url + "/?" + query
 
     #Display the website in base_URL
     else:
@@ -30,22 +29,5 @@ def get_website(request, website):
 
     page = 'website/website.html'
 
-    context['filter_form'] = filter_form
-
     return render_to_response(page, context,
-                              context_instance=RequestContext(request))
-
-
-#Unused function
-def get_filter_form(request, website):
-    print >>sys.stderr, "Inside Get"
-    url = website.series.data_source.load_backend().cursor().url
-    query = website.series.query
-
-    filter_form = FilterForm(website.filterset.all(), request.user)
-    print >>sys.stderr, "got the filter_form"
-
-    return render_to_response('website/filter_website.html',
-                              {'filter_form': filter_form,
-                               'url': url + "/?" + query},
                               context_instance=RequestContext(request))
