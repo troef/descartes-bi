940f91c3f593d6fed31d6d81bf9566468533ce4f
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-08-18 02:21:27 -0400

    Allow changing a serie's datasource from the sources list

--- descartes-bi/apps/reports/admin.py
+++ descartes-bi/apps/reports/admin.py
@@ -153,7 +153,8 @@ class FiltersetAdmin(admin.ModelAdmin):
 class SerieAdmin(admin.ModelAdmin):
     search_fields = ['name', 'label']
     search_fields_verbose = ['Name', 'Label']
-    list_display = ('name', 'label', 'get_params', 'get_parents', 'get_data_source', 'validated')
+    list_display = ('name', 'label', 'get_params', 'get_parents', 'data_source', 'validated')
+    list_editable = ('data_source',)
     order = 2
     fieldsets = (
         (_('General'), {
@@ -181,14 +182,6 @@ class SerieAdmin(admin.ModelAdmin):
         self.message_user(request, _(u"%s copied.") % message_bit)
 
     clone.short_description = _(u"Copy the selected object")
-
-    def get_data_source(self, instance):
-        if hasattr(instance, 'data_source'):
-            return instance.data_source
-        else:
-            return _('None')
-
-    get_data_source.short_description = ('data source')
 
 
 class SerieInline(admin.StackedInline):
