bd709ba013bb50d18108f39cb70cd1c001ee0549
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-08-18 23:58:06 -0400

    Add transactiona nd csrf middlewares

--- descartes_bi/settings.py
+++ descartes_bi/settings.py
@@ -112,9 +112,11 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',
 )
 
 ROOT_URLCONF = 'descartes_bi.urls'
