2799cd7292623a1d1fbac8693c1733ff776a3dfb
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-08-18 23:58:59 -0400

    Add WSGI_APPLICATION setting, unused TEMPLATE_DIRS entries

--- descartes_bi/settings.py
+++ descartes_bi/settings.py
@@ -119,14 +119,13 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.transaction.TransactionMiddleware',
 )
 
-ROOT_URLCONF = 'descartes_bi.urls'
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'libre.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, 'branding'),
-    os.path.join(SITE_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
