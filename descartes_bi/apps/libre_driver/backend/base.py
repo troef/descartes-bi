937d790efac1b0acd2b66ccbcd155f3c2f52f300
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-08-26 03:56:47 -0400

    Add authentication support to the LIBRE DB backend

--- descartes_bi/apps/libre_driver/backend/base.py
+++ descartes_bi/apps/libre_driver/backend/base.py
@@ -59,13 +59,15 @@ class Cursor():
     # TODO: Add support for LIBRE query errors
     # Capture and decode LIBRE HTTP 400 errors
 
-    def __init__(self, host, name, port):
+    def __init__(self, host, name, port, user, password):
         self.url = '%(schema)s%(host)s:%(port)d/api/sources/%(name)s/data' % {
             'schema': 'http://',
             'host': host,
             'port': port or 80,
             'name': name
         }
+        self.user = user
+        self.password = password
 
     def execute(self, query, params=()):
         #TODO: In case we support kwargs bases query and params
@@ -75,7 +77,10 @@ class Cursor():
 
         self.query_url = ''.join([self.url, '?', query_string, '&_format=json'])
         # TODO: use proper URL fabrication
-        self.response = requests.get(self.query_url)
+        if self.username and self.password:
+            self.response = requests.get(self.query_url, auth=(self.user, self.password))
+        else:
+            self.response = requests.get(self.query_url)
 
     def fetchall(self):
         return self.response.json()
@@ -101,7 +106,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
     close = ignore
 
     def cursor(self):
-        return Cursor(self.settings_dict.get('HOST'), self.settings_dict.get('NAME'), self.settings_dict.get('PORT'))
+        return Cursor(self.settings_dict.get('HOST'), self.settings_dict.get('NAME'), self.settings_dict.get('PORT'), self.settings_dict.get('USER'), self.settings_dict.get('PASSWORD'))
 
     def __init__(self, *args, **kwargs):
         super(DatabaseWrapper, self).__init__(*args, **kwargs)
